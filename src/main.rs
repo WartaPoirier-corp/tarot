61d95bf28052bd066262aad511116d43438491a2
Mathis Grange <grangemathis@gmail.com>
2020-05-02 15:46:09 +0200

    Copy-Paste not very goude

--- src/main.rs
+++ src/main.rs
@@ -18,27 +18,21 @@ fn main() -> amethyst::Result<()> {
     let app_root = application_root_dir()?;
     let display_config_path = app_root.join("config").join("display.ron");
 
-    let game_data = GameDataBuilder::default();
+    let game_data = GameDataBuilder::default()
+    .with_bundle(
+        RenderingBundle::<DefaultBackend>::new()
+            // The RenderToWindow plugin provides all the scaffolding for opening a window and drawing on it
+            .with_plugin(
+                RenderToWindow::from_config_path(display_config_path)?
+                    .with_clear([0.0, 0.0, 0.0, 1.0]),
+            )
+            // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
+            .with_plugin(RenderFlat2D::default()),
+    )?;
 
     let assets_dir = app_root.join("assets");
     let mut game = Application::new(assets_dir, LeJeu, game_data)?;
     game.run();
-
-    let app_root = application_root_dir()?;
-
-    let display_config_path = app_root.join("config").join("display.ron");
-
-    let game_data = GameDataBuilder::default()
-        .with_bundle(
-            RenderingBundle::<DefaultBackend>::new()
-                // The RenderToWindow plugin provides all the scaffolding for opening a window and drawing on it
-                .with_plugin(
-                    RenderToWindow::from_config_path(display_config_path)?
-                        .with_clear([0.0, 0.0, 0.0, 1.0]),
-                )
-                // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
-                .with_plugin(RenderFlat2D::default()),
-        )?;
 
     Ok(())
 }
