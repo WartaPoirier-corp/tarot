810d383ec5bee44ff087f5a7b4d983ad96a0406e
Ana Gelez <ana@gelez.xyz>
2020-06-29 21:43:27 +0200

    WIP: main menu

--- game/src/main.rs
+++ game/src/main.rs
@@ -5,7 +5,12 @@ use amethyst::{
         types::DefaultBackend,
         RenderingBundle,
     },
-    utils::application_root_dir,
+    core::transform::TransformBundle,
+    input::{InputBundle, StringBindings},
+    utils::{
+        application_root_dir,
+        fps_counter::FpsCounterBundle,
+    },
 };
 
 mod states;
@@ -17,19 +22,25 @@ fn main() -> amethyst::Result<()> {
     let display_config_path = app_root.join("config").join("display.ron");
 
     let game_data = GameDataBuilder::default()
-    .with_bundle(
-        RenderingBundle::<DefaultBackend>::new()
-            // The RenderToWindow plugin provides all the scaffolding for opening a window and drawing on it
-            .with_plugin(
-                RenderToWindow::from_config_path(display_config_path)?
-                    .with_clear([0.0, 0.0, 0.0, 1.0]),
-            )
-            // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
-            .with_plugin(RenderFlat2D::default()),
-    )?;
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(InputBundle::<StringBindings>::new())?
+        // .with_system_desc(UiEventHandlerSystemDesc::default(), "ui_event_handler", &[])
+        .with_bundle(FpsCounterBundle::default())?
+        .with_bundle(
+            RenderingBundle::<DefaultBackend>::new()
+                // The RenderToWindow plugin provides all the scaffolding for opening a window and drawing on it
+                .with_plugin(
+                    RenderToWindow::from_config_path(display_config_path)?
+                        .with_clear([0.0, 0.0, 0.0, 1.0]),
+                )
+                // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
+                .with_plugin(RenderFlat2D::default())
+                .with_plugin(amethyst::ui::RenderUi::default()),
+        )?
+        .with_bundle(amethyst::ui::UiBundle::<StringBindings>::new())?;
 
     let assets_dir = app_root.join("assets");
-    let mut game = Application::new(assets_dir, states::game::Partie, game_data)?;
+    let mut game = Application::new(assets_dir, states::main_menu::MenuPrincipal, game_data)?;
     game.run();
 
     Ok(())
--- game/src/states/main_menu.rs
+++ game/src/states/main_menu.rs
@@ -1,5 +1,14 @@
 use amethyst::prelude::*;
+use amethyst::ui::UiCreator;
 
 pub struct MenuPrincipal;
 
-impl SimpleState for MenuPrincipal{}
+impl SimpleState for MenuPrincipal {
+    fn on_start(&mut self, data: StateData<GameData>) {
+        let mut world = data.world;
+        
+        world.exec(|mut creator: UiCreator<'_>| {
+            creator.create("ui/menu.ron", ())
+        });
+    }
+}
